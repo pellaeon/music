d11dee6005473357451299dc72b55deb5ecea43a
Pellaeon Lin <nfsmwlin@gmail.com>
2016-01-14 23:08:32 +0800

    Update# add track file url in song stub

--- js/app/stubs/song.js
+++ js/app/stubs/song.js
@@ -1,13 +1,14 @@
 import album from './album';
 
 export default {
-    album,
-    id: null,
-    album_id: 0,
-    title: '',
-    length: 0,
-    fmtLength: '00:00',
-    lyrics: '',
-    playing: false,
-    liked: false,
+	album,
+	id: null,
+	album_id: 0,
+	title: '',
+	length: 0,
+	fmtLength: '00:00',
+	lyrics: '',
+	playing: false,
+	liked: false,
+	files: {},
 };
