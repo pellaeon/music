9dbb208f3fd7794de0fd16b80651ff6af75c2d59
Pellaeon Lin <nfsmwlin@gmail.com>
2016-01-22 17:29:41 +0800

    Fix# vendor css: fontawesome, plyr

--- js/app/gulpfile.js
+++ js/app/gulpfile.js
@@ -9,6 +9,13 @@ elixir.config.assetsPath = '';
 elixir.config.js.folder = '';
 elixir.config.js.outputFolder = '';
 elixir.config.js.browserify.options.debug = true;
+elixir.config.css.folder = '';
+elixir.config.css.outputFolder = '../../css';
 elixir(function (mix) {
     mix.browserify('main.js');
+	mix.copy('vendor/fontawesome/fonts', '../../fonts');
+	mix.styles([
+			'vendor/fontawesome/css/font-awesome.min.css',
+			'vendor/plyr/dist/plyr.css'
+	], '../../css/vendor.css');
 });
